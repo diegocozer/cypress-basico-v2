c3a1f886bd453bbb41c6a58f33c70f50fd202c47
Diego-Cozer <diego.farias@sances.com.br>
2023-04-27 17:22:27 -0300

    caso de teste

--- cypress/integration/sancesweb.spec.js
+++ cypress/integration/sancesweb.spec.js
@@ -13,10 +13,7 @@ describe('Login pedrotti', function(){
         cy.get('[data-cy="loginemail"]').type('diego.farias@sances.com.br')
         cy.get('[data-cy="loginpass"]').type('LARANJAUVA')
         cy.get('[data-cy="btnloginEntrar"]').click()
-        cy.get('.list-selection first p').should('equal','1 - PEDROTTI IMPLEMENTOS').click()
-        cy.get('#pr_id_5_content p').should('1 - PEDROTTI IMPLEMENTOS')
-        cy.get('.layout-profile-link .p-button-label').click()
-        cy.get('.list-selection').should('have.value','PEDROTTI IMPLEMENTOS' )
+        
         
         
     })
